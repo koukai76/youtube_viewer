ad8349aa532210a00ba336b271fa864b7aff9774
koukai76 <61773788+koukai76@users.noreply.github.com>
2021-05-15 13:28:03 +0900

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,34 @@
+const MOVIE = 'movie';
+
+const w = document.body.clientWidth;
+
+const main = () => {
+  if (w > 768) {
+    document.getElementById(MOVIE).style.width = '768px';
+    document.getElementById(MOVIE).style.height = '438px';
+    return;
+  }
+
+  document.getElementById(MOVIE).style.width = w + 'px';
+  document.getElementById(MOVIE).style.height = w * 0.5625 + 'px';
+};
+
+main();
+
+document.getElementById('input').addEventListener('keydown', e => {
+  const key = e.keyCode || e.charCode || 0;
+  if (key !== 13) {
+    return;
+  }
+
+  const url = e.target.value;
+  const rs = url.match(/https:\/\/www.youtube.com\/watch\?v=(.*)$/);
+
+  if (rs[1]) {
+    console.log(rs[1]);
+    document.getElementById(
+      MOVIE
+    ).src = `https://www.youtube.com/embed/${rs[1]}`;
+    return;
+  }
+});
