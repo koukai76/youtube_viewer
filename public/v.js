365219e1b6b4ce7ad6baf6bef8eb463267b2eb28
yu <yu>
2021-05-21 14:20:34 +0900

    a

--- public/v.js
+++ public/v.js
@@ -1,11 +1,15 @@
 const MOVIE = 'movie';
 
-const w = document.body.clientWidth;
-
 const main = () => {
-  if (w > 768) {
-    document.getElementById(MOVIE).style.width = '768px';
-    document.getElementById(MOVIE).style.height = '438px';
+  const url = new URL(window.location.href);
+  const v = url.searchParams.get('v');
+  document.getElementById(MOVIE).src = `https://www.youtube.com/embed/${v}`;
+
+  const w = document.body.clientWidth;
+  if (w > 992) {
+    document.getElementById(MOVIE).style.width = 0.8 * w + 'px';
+    document.getElementById(MOVIE).style.height =
+      (438 / 768) * (0.8 * w) + 'px';
     return;
   }
 
@@ -13,27 +17,4 @@ const main = () => {
   document.getElementById(MOVIE).style.height = w * 0.5625 + 'px';
 };
 
-document.getElementById('input').addEventListener('keydown', e => {
-  const key = e.keyCode || e.charCode || 0;
-  if (key !== 13) {
-    return;
-  }
-
-  const url = e.target.value;
-  const rs = url.match(/https:\/\/www.youtube.com\/watch\?v=(.*)$/);
-
-  if (rs[1]) {
-    document.getElementById(
-      MOVIE
-    ).src = `https://www.youtube.com/embed/${rs[1]}`;
-    return;
-  }
-});
-
-const url = new URL(window.location.href);
-const v = url.searchParams.get('v');
-
-document.getElementById(MOVIE).src = `https://www.youtube.com/embed/${v}`;
-
 main();
-
