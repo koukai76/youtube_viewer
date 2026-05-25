8a5434ce940fad84c896e9f8dcd359bd87dadb17
koukai76 <61773788+koukai76@users.noreply.github.com>
2026-05-25 12:44:34 +0900

    Update v.js

--- public/v.js
+++ public/v.js
@@ -5,16 +5,16 @@ const main = () => {
   const v = url.searchParams.get('v');
   document.getElementById(MOVIE).src = `https://www.youtube.com/embed/${v}`;
 
-  const w = document.body.clientWidth;
-  if (w > 992) {
-    document.getElementById(MOVIE).style.width = 0.8 * w + 'px';
-    document.getElementById(MOVIE).style.height =
-      (438 / 768) * (0.8 * w) + 'px';
-    return;
-  }
+  // const w = document.body.clientWidth;
+  // if (w > 992) {
+  //   document.getElementById(MOVIE).style.width = 0.8 * w + 'px';
+  //   document.getElementById(MOVIE).style.height =
+  //     (438 / 768) * (0.8 * w) + 'px';
+  //   return;
+  // }
 
-  document.getElementById(MOVIE).style.width = w + 'px';
-  document.getElementById(MOVIE).style.height = w * 0.5625 + 'px';
+  // document.getElementById(MOVIE).style.width = w + 'px';
+  // document.getElementById(MOVIE).style.height = w * 0.5625 + 'px';
 };
 
 main();
